0ac4de664355e42e80ebe1df5773240baa93ced8
ashuydv-05 <161949281+ashuydv-05@users.noreply.github.com>
2024-09-30 01:05:20 +0530

    Create program3.cpp

---
+++ program3.cpp
@@ -0,0 +1,45 @@
+// ashu
+// 23070123154
+
+#include <iostream>
+using namespace std;
+
+class fetch {
+    int num;
+public:
+   
+    fetch() {
+        num = 45;
+        cout << num << " 1st" << endl;
+    }
+
+   
+    fetch(int x) {
+        num = x;
+        cout << num << " 2nd" << endl;
+    }
+
+   
+    fetch(const fetch &b) {
+        num = b.num;
+        cout << num << " copy" << endl;
+    }
+
+    
+    void disp() const {
+        cout << num << endl;
+    }
+};
+
+int main() {
+    fetch f1;     
+    fetch f2(25); 
+    fetch f3(f1); 
+
+   
+    f1.disp();
+    f2.disp();
+    f3.disp();
+
+    return 0;
+}
